04c29e7929b7a552e577566268144079de0988fb
Alexander Illescas <49077120+AlexIVBite5@users.noreply.github.com>
2023-02-15 15:08:54 -0500

    prueba unitaria create

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,13 +1,26 @@
-from src.handler import create
 import pytest
+from src.handler import create
+import json
+from datetime import datetime
+from uuid import UUID, uuid4
 
-def test_create():
-    data={
-        "name":"Israel",
-        "last_name":"Lopez"
-    }
-    respuesta_positiva = {
-        "statusCode": 200,
-        "body": "Successfully created."
-    }
-    assert create(event=data)==respuesta_positiva
+
+
+@pytest.mark.parametrize("event, expected_response", [
+    ({"body": '{"name": "Alex", "last_name": "Illescas"}'},
+     {"statusCode": 200, "body": "Successfully created."}),
+    ({"body": '{"name": "Alex", "last_name": "Illescas"}'},
+     {"statusCode": 500, "body": "An error occured while creating post."})
+])
+def test_create(event, expected_response, mocker):
+    # Mock dynamodb.put_item() and dynamo.to_item()
+    dynamodb = mocker.patch("lambda_function.dynamodb")
+    dynamo = mocker.patch("lambda_function.dynamo")
+    dynamo.to_item.return_value = {"id": str(uuid4()), "name": "Alex", "last_name": "Illescas", "createdAt": datetime.now().isoformat()}
+    dynamodb.put_item.return_value = {"ResponseMetadata": {"HTTPStatusCode": 200}}
+
+    # Call create function
+    response = create(event, None)
+
+    # Check response
+    assert response == expected_response
